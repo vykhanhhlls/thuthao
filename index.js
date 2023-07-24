dcec7abed9b1a53ae7d48dec2ddef29359342173
vykhanhhlls <vykhanhhlls@gmail.com>
2023-07-25 00:46:36 +0700

    add hello

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mainContentLetter = "祝你越来越漂亮 ^.^" //Nội dung của bức t
 
 // Gắn 1 đường link ảnh bất kì
 let imgStart = document.querySelector(".myAI"); //Hình ảnh xuất hiện trong lời mở đầu của bức thư
-imgStart.src = "./img/cute-young-boy-kid-wearing-vest-and-hat-free-png.png";
+imgStart.src = "./img/thao1.png";
 
 // Gắn 1 link ảnh bất kì
 let imgLetter = document.querySelector(".img");
